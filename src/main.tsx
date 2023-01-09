a37ec66f4970e85f3215d567f5841ccc2d032dce
jerry <qaz013039@gmail.com>
2023-01-10 00:25:21 +0800

    1. Update typography style guideline. 2. Add PostCSS plugin. 3. Update PostCSS config. 4. Initialize CSS file.

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { RouterProvider } from 'react-router-dom';
 
 import router from '~/router';
 
-import './index.css';
+import '~/style/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
