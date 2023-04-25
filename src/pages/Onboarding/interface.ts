03a9643523a59373b8839b5f0f5b98d4ca3f14b1
jerry <qaz013039@gmail.com>
2023-04-25 19:54:28 +0800

    Fix the user type.

--- src/pages/Onboarding/interface.ts
+++ src/pages/Onboarding/interface.ts
@@ -1,15 +1,16 @@
 import React from 'react';
 
+import { GENDER, AMOUNT, LEVEL } from '~/enums/user';
 import type { Nullable } from '~/typings/utils';
 
 export type User = {
   id?: string;
   height: Nullable<number>;
   weight: Nullable<number>;
-  gender: Nullable<number>;
+  gender: Nullable<GENDER>;
   birth?: Nullable<number>;
-  amount: Nullable<number>;
-  level: Nullable<number>;
+  amount: Nullable<AMOUNT>;
+  level: Nullable<LEVEL>;
   targetWeight?: number;
   name: string;
   login?: string;
