6249fb232699f1eb6d1793e6ba72d434a9b0f9b8
jerry <qaz013039@gmail.com>
2023-04-25 19:57:26 +0800

    Add formula and utilities services.

---
+++ src/services/formula.ts
@@ -0,0 +1,80 @@
+import dayjs from 'dayjs';
+
+import { GENDER, AMOUNT } from '~/enums/user';
+import { LEVEL, CHALLENGE, CHALLENGEWEIGHTED, CHALLENGERANGE } from '~/enums/exercise';
+
+import type { User } from '~/pages/Onboarding/interface';
+import type { Exercise } from '~/static/exercise/data';
+import type { PlanExerciseData } from './exercise';
+
+const getAge = (birth: number): number => {
+  return dayjs(Date.now()).diff(birth, 'year');
+};
+
+export const getBMR = (user: User): number | null => {
+  if (user.weight && user.height && user.birth) {
+    const age = getAge(user.birth);
+
+    switch (user.gender) {
+      case GENDER.MALE:
+        return 88.362 + 13.397 * user.weight + 4.799 * user.height - 5.677 * age;
+      case GENDER.FEMALE:
+        return 447.593 + 9.247 * user.weight + 3.098 * user.height - 4.33 * age;
+      default:
+        return null;
+    }
+  } else {
+    return null;
+  }
+};
+
+export const getTDEE = (user: User): number | null => {
+  const bmr = getBMR(user);
+  const bmrMapping: { [key in AMOUNT]: number } = {
+    [AMOUNT.BEDRIDDEN]: 1.2,
+    [AMOUNT.SEDENTARY]: 1.3,
+    [AMOUNT.LIGHT]: 1.375,
+    [AMOUNT.MODERATE]: 1.55,
+    [AMOUNT.HEAVY]: 1.725,
+    [AMOUNT.ATHLETE]: 1.9,
+  };
+
+  if (bmr && user.amount) {
+    return bmr * bmrMapping[user.amount];
+  } else {
+    return null;
+  }
+};
+
+export const getPlanChallenge = (exerciseList: Array<Exercise & PlanExerciseData>): CHALLENGE => {
+  let beginner = 0;
+  let intermediate = 0;
+  let advanced = 0;
+
+  exerciseList.forEach((exercise) => {
+    switch (exercise.level) {
+      case LEVEL.BEGINNER:
+        beginner++;
+        break;
+      case LEVEL.INTERMEDIATE:
+        intermediate++;
+        break;
+      case LEVEL.ADVANCED:
+        advanced++;
+        break;
+    }
+  });
+
+  const result =
+    beginner * CHALLENGEWEIGHTED.BEGINNER +
+    intermediate * CHALLENGEWEIGHTED.INTERMEDIATE +
+    advanced * CHALLENGEWEIGHTED.ADVANCED;
+
+  if (result < CHALLENGERANGE.ONE) {
+    return CHALLENGE.EASY;
+  } else if (CHALLENGERANGE.ONE <= result && result <= CHALLENGERANGE.TWO) {
+    return CHALLENGE.MEDIUM;
+  } else {
+    return CHALLENGE.HARD;
+  }
+};
