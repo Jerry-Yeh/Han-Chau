3961115c878812143cad5b29cfc57dcec53b0fdc
jerry <qaz013039@gmail.com>
2023-05-21 11:19:18 +0800

    Initialize the Xstate.

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,24 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { interpret } from 'xstate';
+import { inspect } from '@xstate/inspect';
 
 import router from '~/router';
 import { store, persistor } from '~/store';
+import { machine } from './state';
 
 import '~/style/index.scss';
 import './i18n';
 
-// moment.updateLocale('en', {
-//   weekdaysMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-// });
+/** States */
+inspect({
+  // options
+  url: 'https://stately.ai/viz?inspect',
+  // url: 'https://statecharts.io/inspect',
+  iframe: false, // open in new window
+});
+interpret(machine, { devTools: true }).start();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
