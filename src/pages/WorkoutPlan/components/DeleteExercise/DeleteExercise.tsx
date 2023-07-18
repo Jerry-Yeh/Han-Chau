6512a9180b1b1941a4b746d7b942e7a88b8f0d6b
jerry <qaz013039@gmail.com>
2023-07-18 15:42:14 +0800

    Remove useless code.

--- src/pages/WorkoutPlan/components/DeleteExercise/DeleteExercise.tsx
+++ src/pages/WorkoutPlan/components/DeleteExercise/DeleteExercise.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useRef, useEffect } from 'react';
+import React, { Fragment, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useAppSelector, useAppDispatch } from '~/store/hook';
 
