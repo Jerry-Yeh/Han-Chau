608752e277c9617d8ddd01003717e8f6c8ae6b68
jerry <qaz013039@gmail.com>
2024-06-01 13:09:53 +0800

    refactor: Adjust the accordion component. 1. Add different sizes. 2. Temporarily adjust the maximum height to 10000 px.

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -2,8 +2,15 @@ import React, { useState, useRef, useEffect } from 'react';
 import { ChevronDownIcon } from '@heroicons/react/24/outline';
 
 import type { AccordionProps } from '.';
+import { ACCORDION_SIZE } from '.';
 
-const Accordion: React.FC<AccordionProps> = ({ children, label, extra }: AccordionProps) => {
+const Accordion: React.FC<AccordionProps> = ({
+  children,
+  className,
+  label,
+  extra,
+  size = ACCORDION_SIZE.m,
+}: AccordionProps) => {
   const [collapsed, setCollapsed] = useState(false);
   const contentRef = useRef<HTMLDivElement>(null);
   const [contentHeightStyle, setContentHeightStyle] = useState('0');
@@ -12,39 +19,44 @@ const Accordion: React.FC<AccordionProps> = ({ children, label, extra }: Accordi
     setCollapsed((status) => !status);
   };
 
+
   useEffect(() => {
     if (collapsed) {
-      setContentHeightStyle(`${contentRef.current?.scrollHeight}px`);
+      setContentHeightStyle(`10000px`);
     } else {
       setContentHeightStyle('0');
     }
   }, [collapsed]);
 
   return (
-    <div className='px-4'>
+    <div className={className || ''}>
       <div
         onClick={switchStatus}
         aria-hidden='true'
-        className='w-full flex justify-between cursor-pointer py-4'
+        className={`w-full flex justify-between cursor-pointer ${size === ACCORDION_SIZE.m && 'py-3'
+          }`}
       >
         <div className='flex items-center'>
-          <button className='mr-4'>
+          {size === ACCORDION_SIZE.m && (
             <ChevronDownIcon
-              className={`w-6 h-6 ${
-                collapsed ? 'rotate-180' : 'rotate-270'
-              } transition duration-500`}
+              className={`w-6 h-6 ${!collapsed && '-rotate-90'} transition duration-500 mr-4`}
             />
-          </button>
+          )}
           <span>{label}</span>
+          {size === ACCORDION_SIZE.s && (
+            <ChevronDownIcon
+              className={`w-5 h-5 ${!collapsed && '-rotate-90'} transition duration-500 ml-1`}
+            />
+          )}
         </div>
         {extra}
       </div>
       <div
         ref={contentRef}
         style={{
-          height: contentHeightStyle,
+          maxHeight: contentHeightStyle,
         }}
-        className={`w-full overflow-hidden origin-top transition-height duration-500`}
+        className={`w-full overflow-hidden origin-top transition-all duration-500`}
       >
         {children}
       </div>
--- src/components/Accordion/index.ts
+++ src/components/Accordion/index.ts
@@ -2,10 +2,17 @@ import { ReactNode } from 'react';
 
 import HCAccordion from './Accordion';
 
+export enum ACCORDION_SIZE {
+  m,
+  s,
+}
+
 export interface AccordionProps {
   children?: ReactNode;
+  className?: string;
   label: string;
   extra?: ReactNode;
+  size?: ACCORDION_SIZE;
 }
 
 export default HCAccordion;
