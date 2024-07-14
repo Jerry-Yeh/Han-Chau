d3c819e757947778035749045610871fb4acc120
jerry <qaz013039@gmail.com>
2024-07-14 18:27:24 +0800

    [refactor] Adjust the location of the button component props type .

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,42 +1,31 @@
-import React, { ReactNode, MouseEvent } from 'react';
+import React from 'react';
 import classNames from 'classnames';
 import { ArrowLongRightIcon } from '@heroicons/react/20/solid';
 
-interface Props {
-  children?: ReactNode;
-  className?: string;
-  color: 'primary' | 'secondary' | 'tertiary' | 'success' | 'destructive';
-  disabled?: boolean;
-  block?: boolean;
-  textColor?: string;
-  next?: boolean;
-  prefix?: ReactNode;
-  suffix?: ReactNode;
-  onClick?: (e: MouseEvent<HTMLButtonElement>) => void;
-}
+import type { ButtonProps, ClickEvent } from '.';
 
-const HCButton: React.FC<Props> = (props: Props) => {
+const HCButton: React.FC<ButtonProps> = (props: ButtonProps) => {
   const colorVariants = (color: string, disabled = false) =>
-    ({
-      primary: !disabled
-        ? 'bg-highlight hover:bg-highlight-hover active:bg-highlight-pressed text-onColor'
-        : 'bg-highlight-disabled text-disabled',
-      secondary: !disabled
-        ? 'bg-tertiary hover:bg-hover active:bg-pressed text-secondary'
-        : 'bg-disabled text-disabled',
-      tertiary: !disabled
-        ? 'bg-primary text-secondary hover:bg-hover hover:border-hover active:bg-pressed active:border-pressed  border border-secondary'
-        : 'bg-primary text-disabled border border-disabled',
-      success: !disabled
-        ? 'bg-tertiary text-success hover:bg-hover active:bg-pressed'
-        : 'bg-success-disabled text-disabled',
-      destructive: !disabled
-        ? 'bg-destructive-light text-destructive hover:bg-destructive-light-hover active:bg-destructive-light-pressed'
-        : 'bg-destructive-disabled text-disabled',
-    }[color]);
+  ({
+    primary: !disabled
+      ? 'bg-highlight hover:bg-highlight-hover active:bg-highlight-pressed text-onColor'
+      : 'bg-highlight-disabled text-disabled',
+    secondary: !disabled
+      ? 'bg-tertiary hover:bg-hover active:bg-pressed text-secondary'
+      : 'bg-disabled text-disabled',
+    tertiary: !disabled
+      ? 'bg-primary text-secondary hover:bg-hover hover:border-hover active:bg-pressed active:border-pressed  border border-secondary'
+      : 'bg-primary text-disabled border border-disabled',
+    success: !disabled
+      ? 'bg-tertiary text-success hover:bg-hover active:bg-pressed'
+      : 'bg-success-disabled text-disabled',
+    destructive: !disabled
+      ? 'bg-destructive-light text-destructive hover:bg-destructive-light-hover active:bg-destructive-light-pressed'
+      : 'bg-destructive-disabled text-disabled',
+  }[color]);
   const cursorClass = classNames(props.disabled ? 'cursor-not-allowed' : 'cursor-pointer');
 
-  const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
+  const handleClick = (e: ClickEvent) => {
     e.stopPropagation();
     if (props.onClick) {
       props.onClick(e);
--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -1,3 +1,20 @@
+import { ReactNode, MouseEvent } from 'react';
+
 import HCButton from './Button';
+
+export type ClickEvent = MouseEvent<HTMLButtonElement>;
+
+export interface ButtonProps {
+    children?: ReactNode;
+    className?: string;
+    color: 'primary' | 'secondary' | 'tertiary' | 'success' | 'destructive';
+    disabled?: boolean;
+    block?: boolean;
+    textColor?: string;
+    next?: boolean;
+    prefix?: ReactNode;
+    suffix?: ReactNode;
+    onClick?: (e: ClickEvent) => void;
+}
 
 export default HCButton;
