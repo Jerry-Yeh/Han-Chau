b54604a66293eaa6dc41bea0c095a9c1197288a0
jerry <qaz013039@gmail.com>
2025-04-12 18:10:58 +0800

    [fix] Update name of the WorkoutPlan router.

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -9,7 +9,7 @@ import Welcome from '~/pages/Welcome';
 import Onboarding from '~/pages/Onboarding';
 
 /** Plan */
-import Exercise from '~/pages/WorkoutPlan';
+import WorkoutPlan from '~/pages/WorkoutPlan';
 import Plan from '~/pages/WorkoutPlan/Plan';
 import PlanDetail from '~/pages/WorkoutPlan/PlanDetail';
 import Exercises from '~/pages/WorkoutPlan/Exercises';
@@ -17,55 +17,55 @@ import AddRecord from '~/pages/WorkoutPlan/AddRecord';
 import CompletedRecord from '~/pages/WorkoutPlan/CompletedRecord';
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '',
-        element: <Welcome />,
-        children: [{ path: '*', element: <Navigate to='loading' replace /> }],
-      },
-      {
-        path: 'onboarding',
-        element: <Onboarding />,
-      },
-      {
-        path: 'workout-plan',
-        element: <Exercise />,
+    {
+        path: '/',
+        element: <App />,
         children: [
-          { path: '*', element: <Navigate to='Plan' replace /> },
-          { index: true, element: <Plan /> },
-          {
-            path: ':planId',
-            element: <PlanDetail />,
-          },
-          {
-            path: ':planId/:exerciseId',
-            element: <PlanDetail />,
-          },
-          {
-            path: ':planId/record',
-            element: <AddRecord />,
-          },
-          {
-            path: 'exercises',
-            element: <Exercises />,
-            children: [
-              {
-                path: ':exerciseId',
-                element: <Exercises />,
-              },
-            ],
-          },
-          {
-            path: ':planId/record/:recordId',
-            element: <CompletedRecord />,
-          },
+            {
+                path: '',
+                element: <Welcome />,
+                children: [{ path: '*', element: <Navigate to='loading' replace /> }],
+            },
+            {
+                path: 'onboarding',
+                element: <Onboarding />,
+            },
+            {
+                path: 'workout-plan',
+                element: <WorkoutPlan />,
+                children: [
+                    { path: '*', element: <Navigate to='Plan' replace /> },
+                    { index: true, element: <Plan /> },
+                    {
+                        path: ':planId',
+                        element: <PlanDetail />,
+                    },
+                    {
+                        path: ':planId/:exerciseId',
+                        element: <PlanDetail />,
+                    },
+                    {
+                        path: ':planId/record',
+                        element: <AddRecord />,
+                    },
+                    {
+                        path: 'exercises',
+                        element: <Exercises />,
+                        children: [
+                            {
+                                path: ':exerciseId',
+                                element: <Exercises />,
+                            },
+                        ],
+                    },
+                    {
+                        path: ':planId/record/:recordId',
+                        element: <CompletedRecord />,
+                    },
+                ],
+            },
         ],
-      },
-    ],
-  },
+    },
 ]);
 
 export default router;
